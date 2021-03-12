89109014d0f75aceeb4d618b6158fe2eb81c9ff7
fanyonglong <315144604@qq.com>
2021-03-12 08:22:34 +0800

    f

--- src/pages/layoutEditor/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {connect} from 'dva';
-let LayoutEditor=()=>{
-
-    return <div>layout editor</div>
-}
-
-LayoutEditor=connect()(LayoutEditor);
-export default LayoutEditor;
